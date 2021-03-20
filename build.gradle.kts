28b1d9e91438f611942620e9c1f23c1d3c04595d
Valmir Ademar Weide Knasel Junior <valmirknasel@gmail.com>
2021-03-20 02:35:35 -0400

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.4.4"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.3.72"
+    war
+    kotlin("jvm") version "1.4.31"
+    kotlin("plugin.spring") version "1.4.31"
+    kotlin("plugin.jpa") version "1.4.31"
+}
+
+group = "com.knasel.app"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    /* Spring Data JPA */
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    /* Thymeleaf */
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    /* Spring Boot Dev Tools */
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    /* Postgres */
+    runtimeOnly("org.postgresql:postgresql")
+    /* Bootstrap CSS */
+    implementation("org.webjars:bootstrap:4.5.2")
+    implementation("org.webjars:popper.js:2.5.2")
+    implementation("org.webjars:jquery:3.5.1")
+    /* Tomcat */
+    providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+    /* Testes */
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ficha_de_controle"
